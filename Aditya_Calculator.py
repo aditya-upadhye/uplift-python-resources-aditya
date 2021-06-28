64aaa521af64b2e13c144c0c041e6a8197a97da4
Aditya Upadhye <85622937+aditya-upadhye@users.noreply.github.com>
2021-06-28 18:51:04 +0530

    Corrected spellings in Calculator Python Program

--- Aditya_Calculator.py
+++ Aditya_Calculator.py
@@ -2,25 +2,25 @@
 def add(num1, num2):
     return num1 + num2
 
-# The definition of the Substraction fucntuion
+# The definition of the subtraction function
 
 
-def substract(num1, num2):
+def subtract(num1, num2):
     return num1 - num2
 
-# The deficintion of the multiply ficntion
+# The definition of the multiply function
 
 
 def multiply(num1, num2):
     return num1 * num2
 
-# the definiton of the divied efuicntion
+# The definiton of the divide function
 
 
 def divide(num1, num2):
     return num1 / num2
 
-# the definiton fgo the modulo fuicntion
+# The definition of the modulo function
 
 
 def modulo(num1, num2):
@@ -28,14 +28,14 @@ def modulo(num1, num2):
 
 
 print("Calculator")
-print("Write 1 for Addition, 2 for Substraction, 3 for Multiplication, 4 for Division, 5 for Modulo")
+print("Write 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division, 5 for Modulo")
 choice = int(input("Enter your choice"))
 first_num = float(input("Enter the First Number"))
-second_num = float(input("Enter teh second Nmber"))
+second_num = float(input("Enter teh Second Number"))
 if(choice == 1):
     print(add(first_num, second_num))
 if(choice == 2):
-    print(substract(first_num, second_num))
+    print(subtract(first_num, second_num))
 if(choice == 3):
     print(multiply(first_num, second_num))
 if(choice == 4):
