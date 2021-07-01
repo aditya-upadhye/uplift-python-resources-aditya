cd546097ef7fe205930bfdb96f6bc869c61f76ef
Aditya Upadhye <85622937+aditya-upadhye@users.noreply.github.com>
2021-07-01 23:33:14 +0530

    Added Even Odd Program

---
+++ List Problems/smallest_number.py
@@ -0,0 +1,10 @@
+ourList = [10, 20, 4, 45, 79, 99]
+ourList.sort()
+# for Smallest
+print(ourList[0])
+# for largest either
+print(ourList[-1])
+# or
+ourList.sort(reverse=True)
+print(ourList[0])
+print(ourList[1])
